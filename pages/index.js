d892123c81f7330ab9a7746aaaab8e92c76d1a7a
Bilal <mansuribilal101@gmail.com>
2021-09-20 19:57:08 +0530

    ✨ Chore: Initial Setup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="text-8xl">
-      Hello
-    </div>
+    <>
+      <Head>
+        <title>Amazon</title>
+      </Head>
+      <div className="text-8xl">
+        Hello
+      </div>
+    </>
   )
 }
