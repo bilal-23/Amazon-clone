4ee99c054dccba4e34db1fd97a2460766c3d82cd
Bilal <mansuribilal101@gmail.com>
2021-09-24 18:59:00 +0530

    ⚒️ WIP: Header Section

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+    <>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>)
 }
 
 export default MyApp
