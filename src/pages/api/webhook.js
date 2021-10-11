614d2fe3dbe86e50abcee49f2de1f51afa3be941
Bilal <mansuribilal101@gmail.com>
2021-10-11 19:32:51 +0530

    Feat: Webhook to save orders

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,132 +1,68 @@
-import { buffer } from 'micro';
-import * as admin from 'firebase-admin';
+import { buffer } from 'micro'
+import * as admin from 'firebase-admin'
 
-//Secure connection to FIREBASE from the backend
-const serviceAccount = require("../../../permission.json");
-const app = !admin.apps.length ? admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-}) : admin.app();
+//secure firebase connection
+const serviceAccount = require('../../../permission.json')
+const app = !admin.apps.length
+    ? admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    })
+    : admin.app()
 
-//Establish Connextion to STRIPE
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const endpoint = process.env.STRIPE_SIGNING_SECRET;
+// establish connection to stripe
 
-async function fulfillOrder(session) {
-    console.log('fullfilling');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET
 
-    //Store order in firestore
+const fulfillOrder = async (session) => {
+    console.log('fulfilling order', session)
     return app
         .firestore()
         .collection('users')
         .doc(session.metadata.email)
         .collection('orders')
-        .doc(session.id).set({
+        .doc(session.id)
+        .set({
             amount: session.amount_total / 100,
             amount_shipping: session.total_details.amount_shipping / 100,
             images: JSON.parse(session.metadata.images),
             timestamp: admin.firestore.FieldValue.serverTimestamp(),
         })
-        .then(() => console.log('Order success', session.id)).catch((error) => console.log(error.message, 'erroror'))
+        .then(() => {
+            console.log(`SUCCESS ORDER ${session.id} has been added to DB`)
+        })
 }
-
-async function handler(req, res) {
+export default async (req, res) => {
     if (req.method === 'POST') {
-        const requestBuffer = await buffer(req);
-        const payload = requestBuffer.toString();
-        const sig = req.headers["stripe-signature"];
+        const requestBuffer = await buffer(req)
+        const payload = requestBuffer.toString()
+        const sig = req.headers['stripe-signature']
 
-        let event;
-
-        //Verify that EVENT posted is from stripe
+        let event
+        //verify that the event POSTED came from stripe
         try {
-            event = stripe.webhooks.constructEvent(payload, sig, endpoint);
-        } catch (error) {
-            console.log('ERROR', error.message);
-            return res.status(400).send('Webhook Error', error.message);
+            event = stripe.webhooks.constructEvent(payload, sig, endpointSecret)
+        } catch (err) {
+            return res.status(400).send(`Webhook error: ${err.message}`)
         }
 
-        //Hanlder checkout session completed event
+        //handle the checkout session completed
         if (event.type === 'checkout.session.completed') {
-            const session = event.data.object;
+            const session = event.data.object
 
-            //Fulfill the order
-            return fulfillOrder(session).then(() => res.status(200)).catch((err) => res.status(400).json({ error: err.message }))
+            //fulfill the order
+            return fulfillOrder(session)
+                .then(() => res.status(200))
+                .catch((err) => {
+                    res.status(400).send(`webhook error: ${err.message}`)
+                })
         }
     }
 }
-export default handler
 
-//No idea what it does.
 export const config = {
     api: {
-        bodyParse: false,
+        bodyParser: false,
         externalResolver: true,
-    }
+    },
 }
-
-// import { buffer } from 'micro'
-// import * as admin from 'firebase-admin'
-
-// // Secure a connection to firebase
-// const serviceAccount = require('../../../permission.json')
-// const app = !admin.apps.length ? admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// }) : admin.app()
-
-// // Stripe
-
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-
-// const endpointSecurit = process.env.STRIPE_SIGNING_SECRET
-
-// const fullfillOrder = async (session) => {
-//     console.log('Fullfilling Order!!!')
-
-//     return app
-//         .firestore()
-//         .collection('users')
-//         .doc(session.metadata.email)
-//         .collection('orders')
-//         .doc(session.id)
-//         .set({
-//             amount: session.amount_total / 100,
-//             amount_shipping: session.total_details_amount_shipping / 100,
-//             images: JSON.parse(session.metadata.images),
-//             title: JSON.parse(session.metadata.titles),
-//             timestamp: admin.firestore.FieldValue.serverTimestamp()
-//         })
-//         .then(() => {
-//             console.log(`SUCCESS: Order ${session.id} has been added to DB!`)
-//         }).catch(err => console.log(err.message))
-// }
-
-// export default async (req, res) => {
-//     if (req.method === 'POST') {
-//         const requestBuffer = await buffer(req)
-//         const payload = requestBuffer.toString()
-//         const sig = req.headers["stripe-signature"]
-
-//         let event;
-
-//         // Verify (came from stripe)
-//         try {
-//             event = await stripe.webhooks.constructEvent(payload, sig, endpointSecurit)
-//         } catch (e) {
-//             console.log('ERROR', e.message)
-//             return res.status(400).send({ message: "Webhook error: " + e.message })
-//         }
-//         if (event.type === "checkout.session.completed") {
-//             const session = event.data.object
-
-//             // Fullfill the order
-//             return fullfillOrder(session).then(() => res.status(200)).catch(e => res.status(400).send({ message: "WEBHOOK_ERROR: " + e.message }))
-//         }
-//     }
-// }
-
-// export const config = {
-//     api: {
-//         bodyParser: false,
-//         externalResolver: true
-//     }
-// }
